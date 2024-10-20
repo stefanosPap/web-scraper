78fc5ee86a05fba8d9ba851a66a3e5eb2d117cdd
stefanosPap <stefanospapadam@gmail.com>
2024-10-20 21:49:22 +0300

    Move dummy test files

---
+++ tests/dummy.py
@@ -0,0 +1,10 @@
+def square(x: float) -> float:
+    return x*x 
+
+
+class C:
+    def f(self, x):
+        return 2 * x 
+    
+    def g(self, x):
+        return 3 * x
---
+++ tests/dummy_test.py
@@ -0,0 +1,24 @@
+import sys 
+import dummy  
+import pytest  
+
+@pytest.mark.parametrize(
+        ('input_n', 'expected'),
+        (
+            (5, 25),
+            (3., 9.),
+            (1., 1.),
+            (2., 4.),
+            (6., 36.)
+        )
+)
+
+
+
+def test_square(input_n, expected):
+    assert dummy.square(input_n) == expected 
+
+# @pytest.mark.great
+# def test_square(input_value):
+#     assert dummy.square(input_value) == 9 
+
